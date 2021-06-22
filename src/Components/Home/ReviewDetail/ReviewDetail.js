c48f4d8a92de6f3c0e76d107ad14b51dac770c6a
mhhasanmehedi <mmmehedi53@gmail.com>
2021-06-22 12:35:29 +0600

    something change

--- src/Components/Home/ReviewDetail/ReviewDetail.js
+++ src/Components/Home/ReviewDetail/ReviewDetail.js
@@ -5,11 +5,11 @@ const ReviewDetail = (props) => {
     const { name, review,company,photo } = props.review;
     return (
         <div className="col-md-4">
-            <div className="single-review-box text-center">
+            <div className="single-review-box text-center p-3">
                 <img src={photo || "https://i.ibb.co/5GzXkwq/user.png"} alt="" />
                 <h4 style={{marginBottom:"0px"}}>{name}</h4>
                 <small className="text-secondary">{company}</small>
-                <p>{review}</p>
+                <p className="scrollBar">{review}</p>
             </div>
         </div>
     );
